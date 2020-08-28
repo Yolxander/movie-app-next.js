0d78e0da82f22fbf152e9ca114a444b1dd5ce331
Yolxi <jacacanada@gmail.com>
2020-08-28 16:46:26 -0400

    transformed movieList into funcional compnent

--- components/movieList.js
+++ components/movieList.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import Link from 'next/link';
 
-class MovieList extends React.Component {
-	shorten = (text, maxLength) => {
+import { getMovies } from '../actions';
+
+const MovieList = (props) => {
+	const { movies } = props;
+	const shorten = (text, maxLength) => {
 		if (text && text.length > maxLength) {
 			return text.substr(0, maxLength) + '...';
 		}
@@ -10,36 +13,35 @@ class MovieList extends React.Component {
 		return text;
 	};
 
-	renderMovies(movies) {
-		return movies.map((movie) => (
-			<div key={movie.id} className="col-lg-4 col-md-6 mb-4">
-				<div className="card h-100">
-					<Link href="/movies/[id]" as={`/movies/${movie.id}`}>
-						<a>
-							<img className="card-img-top" src={movie.image} alt="" />
-						</a>
-					</Link>
-					<div className="card-body">
-						<h4 className="card-title">
-                        <Link href="/movies/[id]" as={`/movies/${movie.id}`}>
-								<a>{movie.name}</a>
-							</Link>
-						</h4>
-						<p className="card-text">{this.shorten(movie.description, 200)}</p>
-					</div>
-					<div className="card-footer">
-						<small className="text-muted">{movie.rating}</small>
-					</div>
+	return movies.map((movie) => (
+		<div key={movie.id} className="col-lg-4 col-md-6 mb-4">
+			<div className="card h-100">
+				<Link href="/movies/[id]" as={`/movies/${movie.id}`}>
+					<a>
+						<img className="card-img-top" src={movie.image} alt="" />
+					</a>
+				</Link>
+				<div className="card-body">
+					<h4 className="card-title">
+						<Link href="/movies/[id]" as={`/movies/${movie.id}`}>
+							<a>{movie.name}</a>
+						</Link>
+					</h4>
+					<p className="card-text">{shorten(movie.description, 200)}</p>
+				</div>
+				<div className="card-footer">
+					<small className="text-muted">{movie.rating}</small>
 				</div>
 			</div>
-		));
-	}
+		</div>
+	));
+};
 
-	render() {
-		const { movies } = this.props;
-
-		return <React.Fragment>{this.renderMovies(movies)}</React.Fragment>;
-	}
-}
+MovieList.getInitialProps = async () => {
+	const movies = await getMovies();
+	return {
+		movies,
+	};
+};
 
 export default MovieList;
